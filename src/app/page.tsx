8380ea3678d3a3f99ece46f4446b132c0449a4e3
Isola Pelumi <pelumiisola87@gmail.com>
2025-04-28 13:15:36 +0100

    fix: routes issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
 "use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Hello
-    </div>
-  );
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/auth/sign-in");
+  }, [router]);
+
+  return null; // Optionally, you can return a loading spinner or nothing
 }
